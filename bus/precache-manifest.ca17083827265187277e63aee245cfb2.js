aadabc6da305d3c7d6cf5e69c10e48977168117a
Klaas Kliffen <klaasyk@users.noreply.github.com>
2020-04-01 22:14:47 +0200

    Initial bus

---
+++ bus/precache-manifest.ca17083827265187277e63aee245cfb2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0b021022bd25c20cac0a5debfd6fcb",
+    "url": "/bus/index.html"
+  },
+  {
+    "revision": "fdde10d881af41b9def2",
+    "url": "/bus/static/js/0.cda8ad3f.chunk.js"
+  },
+  {
+    "revision": "a067d5badfeead732797",
+    "url": "/bus/static/js/3.89bda44a.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/bus/static/js/3.89bda44a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60b060df30eefe5badb",
+    "url": "/bus/static/js/4.774e2814.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/bus/static/js/4.774e2814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0cbfdc6c8032d7a7e5",
+    "url": "/bus/static/js/5.efdef43d.chunk.js"
+  },
+  {
+    "revision": "68e7c9bee03284f23d0e",
+    "url": "/bus/static/js/6.1dcc90fc.chunk.js"
+  },
+  {
+    "revision": "7fa9d391dcd7367922ea",
+    "url": "/bus/static/js/main.1a44ae42.chunk.js"
+  },
+  {
+    "revision": "e6746d671882a9b2a342",
+    "url": "/bus/static/js/runtime-main.ee184e38.js"
+  }
+]);
